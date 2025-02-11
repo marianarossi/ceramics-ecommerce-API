6ea6c52d348ce01bb503fd4941d8104a79ca6c7d
Mariana Rossi <wmarianarossi@gmail.com>
2025-02-11 19:53:47 -0300

    edit user find endpoint

--- src/main/java/mariana/thePotteryPlace/service/impl/UserServiceImpl.java
+++ src/main/java/mariana/thePotteryPlace/service/impl/UserServiceImpl.java
@@ -2,22 +2,33 @@ package mariana.thePotteryPlace.service.impl;
 
 import mariana.thePotteryPlace.model.User;
 import mariana.thePotteryPlace.repository.UserRepository;
+import mariana.thePotteryPlace.service.AuthService;
 import mariana.thePotteryPlace.service.IUserService;
-import org.modelmapper.ModelMapper;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
 public class UserServiceImpl extends CrudServiceImpl<User, Long> implements IUserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthService authService;
 
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, ModelMapper modelMapper) {
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthService authService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authService = authService;
+    }
+
+    public List<User> findUserByAuth() {
+        Long authenticatedUserId = authService.getAuthenticatedUser().getId();
+        User user = userRepository.findById(authenticatedUserId).orElseThrow(() -> new RuntimeException("User not found"));
+        return List.of(user);
     }
 
     @Override
