31df11c907ba06e525c49c0d2b16bcb32a994327
Mariana Rossi <wmarianarossi@gmail.com>
2025-02-15 23:50:08 -0300

    change user update endpoint

--- src/main/java/mariana/thePotteryPlace/controller/UserController.java
+++ src/main/java/mariana/thePotteryPlace/controller/UserController.java
@@ -2,10 +2,11 @@ package mariana.thePotteryPlace.controller;
 
 import jakarta.validation.Valid;
 import mariana.thePotteryPlace.dto.response.ResponseUserDTO;
-import mariana.thePotteryPlace.dto.request.UserDTO;
+import mariana.thePotteryPlace.dto.update.UserUpdateDTO;
 import mariana.thePotteryPlace.model.User;
 import mariana.thePotteryPlace.service.ICrudService;
 import mariana.thePotteryPlace.service.IUserService;
+import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,14 +18,14 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("users")
-public class UserController extends CrudController<User, UserDTO, ResponseUserDTO, Long> {
+public class UserController extends CrudController<User, UserUpdateDTO, ResponseUserDTO, Long> {
 
     private final IUserService userService;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
 
     public UserController(IUserService userService, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
-        super(User.class, UserDTO.class, ResponseUserDTO.class);
+        super(User.class, UserUpdateDTO.class, ResponseUserDTO.class);
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
         this.modelMapper = new ModelMapper();
@@ -39,26 +40,24 @@ public class UserController extends CrudController<User, UserDTO, ResponseUserDT
     }
 
     @Override
-    public ResponseEntity<ResponseUserDTO> update(@PathVariable Long id, @RequestBody @Valid UserDTO dto) {
+    public ResponseEntity<ResponseUserDTO> update(@PathVariable Long id, @RequestBody @Valid UserUpdateDTO dto) {
         User existingUser = getService().findOne(id);
 
         if (existingUser == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
 
-        // Map the UserDTO to the existing User entity
+        modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+
         modelMapper.map(dto, existingUser);
 
-        // Check if a new password is provided, and if so, encrypt it
         if (dto.getPassword() != null && !dto.getPassword().isEmpty()) {
             String encryptedPassword = passwordEncoder.encode(dto.getPassword());
-            existingUser.setPassword(encryptedPassword);  // Set the encrypted password
+            existingUser.setPassword(encryptedPassword);
         }
 
-        // Save the updated user entity
         User updatedUser = getService().save(existingUser);
 
-        // Convert the updated entity to Response DTO
         ResponseUserDTO responseDTO = modelMapper.map(updatedUser, ResponseUserDTO.class);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
